764c7562e4387f08d7d747a208ecc00695c42868
Adam Derewecki <derewecki@gmail.com>
2012-02-20 14:28:01 -0800

    rails new openscience

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openscience::Application.config.secret_token = 'af7c846d9265810ceb22614f365f51a7321d10d305a7fd0608ed5b058bb0067e64752ef006ff2e5c3aea070494aa92d9a107f404b302a7e40876a1abcc458f74'
