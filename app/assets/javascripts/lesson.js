236477ce2d83909e148dfa1615980a408e12854e
Adam Derewecki <derewecki@gmail.com>
2012-03-11 21:23:24 -0700

    Dynamic section loading

---
+++ app/assets/javascripts/lesson.js
@@ -0,0 +1,13 @@
+$(function() {
+  var picker = $('#section-picker li');
+  picker.find('a').click(function(ev) {
+    ev.preventDefault();
+    var sectionId = $(ev.target).data('section-id');
+    $('.section').hide();
+    $('#' + sectionId).show();
+  });
+  picker.find('a.showall').click(function(ev) {
+    ev.preventDefault();
+    $('.section').show();
+  });
+});
--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -1,5 +1,8 @@
 class LessonController < ApplicationController
   def show
+    @sections = %w(what_is_electricity conductors_and_insulators circuits wire
+                   switches wire_a_house make_a_motor ac_dc make_a_battery
+                   fruit_battery thermocouple)
   end
 
   def electricity
