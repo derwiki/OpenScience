7888d029f8b8a43e8884cd0d0fca3443d6f5773c
Adam Derewecki <adam@causes.com>
2012-04-22 18:30:29 -0700

    Finish matter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def superplus
+    "&#8314;".html_safe
+  end
+
+  def proton
+    "p&#8314;".html_safe
+  end
+
+  def superminus
+    "&#8315;".html_safe
+  end 
+
+  def electron
+    "e&#8315;".html_safe
+  end
+
+  def superzero
+    "&#8304;".html_safe
+  end
+
+  def neutron
+    "n&#8304;".html_safe
+  end
 end
--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,2 +1,9 @@
 module LessonHelper
+  def atomic_notation(element, atomic_number=nil, atomic_weight=nil)
+    content_tag(:span, safe_join(
+      [(content_tag(:sup, atomic_number) if atomic_number),
+       element,
+       (content_tag(:sub, atomic_weight) if atomic_weight)].compact),
+      :class => "element-example")
+  end
 end
